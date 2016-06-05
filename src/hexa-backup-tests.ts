78fee1909b8086bf2de7747a911971ef1a0abdc7
Arnaud Tournier <ltearno@gmail.com>
2016-06-05 11:27:34 +0200

    store

--- src/hexa-backup-tests.ts
+++ src/hexa-backup-tests.ts
@@ -2,6 +2,42 @@ import fs = require('fs');
 import crypto = require('crypto');
 import fsPath = require('path');
 
+
+async function openFile(fileName: string, flags: string) {
+    return new Promise<number>((resolve, reject) => {
+        fs.open(fileName, flags, (err, fd) => {
+            if (err)
+                reject(err);
+            else
+                resolve(fd);
+        });
+    });
+}
+
+async function readFile(fd: number, offset: number, length: number) {
+    return new Promise<Buffer>((resolve, reject) => {
+        let buffer = new Buffer(length);
+
+        fs.read(fd, buffer, 0, length, offset, (err, bytesRead, buffer) => {
+            if (err || bytesRead != length)
+                reject(`error reading file`);
+            else
+                resolve(buffer);
+        });
+    });
+}
+
+async function closeFile(fd: number) {
+    return new Promise<void>((resolve, reject) => {
+        fs.close(fd, (err) => {
+            if (err)
+                reject(err);
+            else
+                resolve();
+        })
+    });
+}
+
 function hashString(value: string) {
     let hash = crypto.createHash('sha256');
     hash.update(value);
@@ -172,10 +208,153 @@ class HexaBackupReader {
             resolve(result);
         });
     }
+
+    async sendSnapshotToStore(store: HexaBackupStore) {
+        let desc = await this.readDirectoryState();
+
+        for (let fileDesc of desc.files) {
+            if (fileDesc.isDirectory)
+                continue;
+
+            let fullFileName = fsPath.join(this.rootPath, fileDesc.name);
+
+            let currentSize = await store.hasShaBytes(fileDesc.contentSha);
+            let stat = fs.lstatSync(fullFileName);
+
+            if (currentSize < stat.size) {
+                console.log(`need to send ${stat.size - currentSize} bytes for file ${fileDesc.name}`);
+
+                const maxBlockSize = 4096;
+
+                let fd = await openFile(fullFileName, 'r');
+
+                let currentReadPosition = currentSize;
+
+                while (currentReadPosition < stat.size) {
+                    let chunkSize = stat.size - currentReadPosition;
+                    if (chunkSize > maxBlockSize)
+                        chunkSize = maxBlockSize;
+
+                    if (chunkSize > 0) {
+                        let buffer = await readFile(fd, currentReadPosition, chunkSize);
+
+                        await store.putShaBytes(fileDesc.contentSha, currentReadPosition, buffer);
+
+                        currentReadPosition += buffer.length;
+                    }
+                }
+
+                await closeFile(fd);
+
+                let validated = await store.validateSha(fileDesc.contentSha);
+                if (!validated) {
+                    console.log(`error uploading ${fileDesc.name}, file content might have changed during uploading the content...`);
+                }
+            }
+
+            // push file descriptor to store
+            // store checks that SHA is valid after the transfer => file state is committed in a consistent way (name, modifDate, contentSha)
+        }
+    }
 }
 
-// object repository
-// reference repository
+class ObjectRepository {
+    private rootPath: string;
+
+    constructor(rootPath: string) {
+        this.rootPath = fsPath.resolve(rootPath);
+        if (!fs.existsSync(this.rootPath))
+            fs.mkdirSync(this.rootPath);
+    }
+
+    /**
+     * Returns the number of bytes that are currently committed for the specified sha
+     */
+    async hasShaBytes(sha: string) {
+        return new Promise<number>((resolve, reject) => {
+            let contentFileName = this.contentFileName(sha);
+            if (fs.existsSync(contentFileName))
+                resolve(fs.lstatSync(contentFileName).size);
+            else
+                resolve(0);
+        });
+    }
+
+    async putShaBytes(sha: string, offset: number, data: Buffer): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            let contentFileName = this.contentFileName(sha);
+            fs.open(contentFileName, 'a', (err, fd) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                fs.write(fd, data, 0, data.byteLength, offset, (err, written, buffer) => {
+                    fs.close(fd, (err) => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+
+                        resolve();
+                    });
+                });
+            });
+        });
+    }
+
+    async validateSha(sha: string) {
+        return new Promise<boolean>(async (resolve, reject) => {
+            let contentFileName = this.contentFileName(sha);
+            let storedContentSha = await hashFile(contentFileName);
+            if (storedContentSha == sha) {
+                resolve(true);
+            }
+            else {
+                fs.unlink(contentFileName, (err) => {
+                    resolve(false);
+                });
+            }
+        });
+    }
+
+    private contentFileName(sha: string) {
+        return fsPath.join(this.rootPath, `${sha}`);
+    }
+}
+
+class HexaBackupStore {
+    private rootPath: string;
+    private objectRepository: ObjectRepository;
+
+    constructor(rootPath: string) {
+        this.rootPath = fsPath.resolve(rootPath);
+
+        this.objectRepository = new ObjectRepository(fsPath.join(this.rootPath, '.hb-object'));
+    }
+
+    startSnapshotTransaction(): string {
+        // create space for storing the transaction
+        return 'id_tx';
+    }
+
+    async hasShaBytes(sha: string) {
+        return this.objectRepository.hasShaBytes(sha);
+    }
+
+    async putShaBytes(sha: string, offset: number, data: Buffer) {
+        return this.objectRepository.putShaBytes(sha, offset, data);
+    }
+
+    async validateSha(sha: string) {
+        return this.objectRepository.validateSha(sha);
+    }
+
+    commitTransaction(idTransaction: string) {
+        // ensure state is consistent
+        // write state info
+    }
+}
 
 async function run() {
     console.log("Test load for Hexa-Backup !");
@@ -188,17 +367,21 @@ async function run() {
     let files = await readDir(dirName);
     console.log(`files: ${files.join()}`);
 
-    //let desc = await analyseDirectory(dirName);
-    //console.log(`descriptor: ${JSON.stringify(desc)}`);
-
     let shaCache = new ShaCache(`d:\\tmp\\.hb-cache`);
     let shaContent = await shaCache.hashFile('d:\\tmp\\CCF26012016.png');
     shaCache.flushToDisk();
     console.log(`picture sha : ${shaContent}`);
 
-    let reader = new HexaBackupReader('d:\\tmp');
+    let backupedDirectory = `D:\\Tmp\\Conseils d'Annelise pour la prochaine AG`;
+
+    let reader = new HexaBackupReader(backupedDirectory);
     let desc = await reader.readDirectoryState();
     console.log(`descriptor: ${JSON.stringify(desc)}`);
+
+    let store = new HexaBackupStore(`D:\\Tmp\\HexaBackupStore`);
+
+    await reader.sendSnapshotToStore(store);
+    console.log('snapshot sent !');
 }
 
 run();
