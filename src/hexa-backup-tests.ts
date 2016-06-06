2b2026b65176a7ad5cbb7d377cdedfec41ae3ad9
Arnaud Tournier <ltearno@gmail.com>
2016-06-06 11:29:04 +0200

    rpc communication

--- src/hexa-backup-tests.ts
+++ src/hexa-backup-tests.ts
@@ -1,10 +1,264 @@
 import { HexaBackupReader } from './HexaBackupReader';
 import { HexaBackupStore } from './HexaBackupStore';
-//import { ClientState, Commit, DirectoryDescriptor, FileDescriptor } from 'Model';
 
-run();
+import fs = require('fs');
+
+interface YoupiService {
+    sayHello(message: string, object: any, buffer: Buffer): Promise<string>;
+}
+
+class GrowingBuffer {
+    private buffer: Buffer;
+
+    constructor() {
+        this.buffer = Buffer.alloc(100);
+    }
+
+    extract(size: number) {
+        let result = Buffer.alloc(size);
+        this.buffer.copy(result, 0, 0, size);
+        return result;
+    }
+
+    writeByte(offset: number, byte: number): number {
+        this.checkEnoughBuffer(offset + 1);
+        this.buffer.writeInt8(byte, offset);
+        return 1;
+    }
+
+    writeUInt32(offset: number, value: number): number {
+        this.checkEnoughBuffer(offset + 4);
+        this.buffer.writeUInt32LE(value, offset);
+        return 4;
+    }
+
+    writeBuffer(offset: number, buffer: Buffer): number {
+        this.checkEnoughBuffer(offset + 4 + buffer.length);
+        this.writeUInt32(offset, buffer.length);
+        buffer.copy(this.buffer, offset + 4, 0, buffer.length);
+        return 4 + buffer.length;
+    }
+
+    writeAny(offset: number, value: any): number {
+        let payload = JSON.stringify(value);
+        let buffer = new Buffer(payload, 'utf8');
+
+        return this.writeBuffer(offset, buffer);
+    }
+
+    private checkEnoughBuffer(size: number) {
+        if (this.buffer.length < size) {
+            console.log('reallocating the buffer');
+            let newBuffer = Buffer.alloc(size * 2);
+            this.buffer.copy(newBuffer, 0, 0, this.buffer.length);
+            this.buffer = newBuffer;
+        }
+    }
+}
+
+const TYPE_BUFFER = 0;
+const TYPE_ANY = 1;
+
+function serializeToBuffer(args: any[]) {
+    console.log(`serializing ${args.length} arguments`);
+
+    let currentOffset = 0;
+    let buffer: GrowingBuffer = new GrowingBuffer();
+    currentOffset += buffer.writeByte(currentOffset, args.length);
+
+    for (let p in args) {
+        let arg = args[p];
+
+        if (arg instanceof Buffer) {
+            currentOffset += buffer.writeByte(currentOffset, TYPE_BUFFER);
+            currentOffset += buffer.writeBuffer(currentOffset, arg);
+        }
+        else {
+            currentOffset += buffer.writeByte(currentOffset, TYPE_ANY);
+            currentOffset += buffer.writeAny(currentOffset, arg);
+        }
+    }
+
+    let result = buffer.extract(currentOffset);
+
+    console.log(`payload serialized to ${result.length} bytes`);
+
+    return result;
+}
+
+function deserializeFromBuffer(buffer: Buffer): any[] {
+    let currentOffset = 0;
+
+    let nbItems = buffer.readUInt8(currentOffset);
+    currentOffset += 1;
+
+    let result = new Array(nbItems);
+    for (let i = 0; i < nbItems; i++) {
+        let itemType = buffer.readUInt8(currentOffset);
+        currentOffset += 1;
+
+        let chunkSize = buffer.readUInt32LE(currentOffset);
+        currentOffset += 4;
+
+        let param = null;
+
+        if (itemType == TYPE_BUFFER) {
+            param = buffer.slice(currentOffset, currentOffset + chunkSize);
+            currentOffset += chunkSize;
+        }
+        else if (itemType == TYPE_ANY) {
+            param = JSON.parse(buffer.slice(currentOffset, currentOffset + chunkSize).toString('utf8'));
+
+            currentOffset += chunkSize;
+        }
+
+        result[i] = param;
+    }
+
+    return result;
+}
+
+let callInfos: { [key: string]: { resolver; rejecter; } } = {};
+
+function createProxy(socket) {
+    let handler = {
+        get(target, propKey, receiver) {
+            return function (...args) {
+                return new Promise((resolve, reject) => {
+                    args = args.slice();
+
+                    args.unshift(propKey);
+
+                    let callId = '_id_' + Date.now();
+                    args.unshift(callId);
+
+                    let payload = serializeToBuffer(args);
+
+                    callInfos[callId] = {
+                        resolver: resolve,
+                        rejecter: reject
+                    };
+
+                    socket.send(payload);
+                });
+            };
+        }
+    };
+    return new Proxy({}, handler);
+}
 
 async function run() {
+
+    let source = ["salut poto", { tat: 'titi' }, new Buffer([1, 5, 7, 8])];
+    let sourcePayload = serializeToBuffer(source);
+    console.log(`sourcePayload : ${sourcePayload.length}`);
+
+    let received = deserializeFromBuffer(sourcePayload);
+    console.log(`received ${received.length} arguments ${JSON.stringify(received)}`);
+
+    //process.exit();
+
+    var engine = require('engine.io');
+    var server = engine.listen(5005);
+
+    let serviceImpl: YoupiService = {
+        sayHello: async function (message: string, object: any, buffer?: Buffer): Promise<string> {
+            return `bonjour, ton message '${message}', ton object : '${JSON.stringify(object)}' et ton buffer fait ${buffer.length} octets`;
+        }
+    };
+
+    server.on('connection', function (socket) {
+        socket.on('message', function (...args) {
+            if (args == null || args.length != 1) {
+                console.log(`received corrupted message !`);
+                return;
+            }
+
+            let received = deserializeFromBuffer(args[0]);
+
+            let callId = received[0];
+            received.shift();
+            let method = received[0];
+            received.shift();
+
+            console.log(`received message callId:${callId} method:${method}`);
+            for (let i = 0; i < received.length; i++) {
+                console.log(` -param ${i} : ${JSON.stringify(received[i])}`);
+            }
+            console.log('calling implementation...');
+
+            let promise: Promise<any> = serviceImpl[method].apply(serviceImpl, received);
+            promise.then((value) => {
+                console.log(`implementation returned ${value}`);
+
+                let result = [callId, value];
+                let resultSerialized = serializeToBuffer(result);
+                socket.send(resultSerialized);
+            });
+
+        });
+    });
+
+    var socket = require('engine.io-client')('ws://localhost:5005');
+    socket.on('open', async function () {
+        socket.on('message', function (data) {
+            let response = deserializeFromBuffer(data);
+            let callId = response[0];
+            let returnValue = response[1];
+
+            let callInfo = callInfos[callId];
+
+            delete callInfos[callId];
+
+            console.log(`client received ${JSON.stringify(response)}`);
+
+            callInfo.resolver(response);
+        });
+        socket.on('close', function () {
+            console.log('client connection closed');
+        });
+
+        let proxy: YoupiService = createProxy(socket);
+
+        let result = await proxy.sayHello("salut poto", { tat: 'titi' }, new Buffer([1, 5, 7, 8]));
+
+        console.log(`reply: ${result}`);
+
+    });
+}
+
+async function runDnode() {
+    var dnode = require('dnode');
+    var server = dnode({
+        transform: function (s, cb) {
+            cb(s.replace(/[aeiou]{2,}/, 'oo').toUpperCase())
+        },
+
+        putBytes: function (obj, chunk: Buffer, cb) {
+            cb(`ok received ${chunk.length}, obj:${JSON.stringify(obj)}`);
+        }
+    });
+    server.listen(5004);
+
+    var d = dnode.connect(5004);
+    d.on('remote', async function (remote) {
+        let input = fs.createReadStream("D:\\Tmp\\Conseils d'Annelise pour la prochaine AG\\bilanmoral2013.docx");
+
+        input.on('data', chunk => {
+            console.log(`send ${chunk.length} bytes.`);
+            remote.putBytes({ field: 'toto' }, chunk, function (s) {
+                console.log('putBytes => ' + s);
+                d.end();
+            });
+        }).on('end', () => {
+            console.log("end of file !");
+        }).on('error', () => {
+            console.log("error in file !");
+        });
+    });
+}
+
+async function runa() {
     console.log("Test load for Hexa-Backup !");
 
     let backupedDirectory = `D:\\Tmp\\Conseils d'Annelise pour la prochaine AG`;
@@ -66,3 +320,5 @@ async function old() {
     shaCache.flushToDisk();
     console.log(`picture sha : ${shaContent}`);*/
 }
+
+run();
