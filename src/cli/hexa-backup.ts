a68d3de854d24dea0ad2262e8aee622444d18f3b
Arnaud Tournier <ltearno@gmail.com>
2016-06-11 14:22:36 +0200

    cli

--- src/cli/hexa-backup.ts
+++ src/cli/hexa-backup.ts
@@ -2,8 +2,21 @@ const log = require('../Logger')('hexa-backup');
 log.conf('dbg', false);
 
 async function run() {
-    let args = process.argv.slice(2).join(" ");
-    log(args);
+    let args = process.argv.slice(2);
+    log(JSON.stringify(args));
+
+    /*
+
+    (*) : avec sourceId(default=hostname-directoryname) & store_address (default=localhost:5005)
+    on peut trouver les valeurs par defaut dans le fichier .hexa-backup.config si présent
+
+    history (*)
+    show current [PREFIX] (*)
+    show commit COMMIT_SHA (*)
+    ls DIRECTORY_DESCRIPTOR_SHA [PREFIX] (*)
+    push PUSHED_DIRECTORY(default=.) (*)
+    store [STORE_DIRECTORY(default=.)]
+    */
 }
 
 run();
