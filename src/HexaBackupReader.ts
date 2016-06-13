6bdd084ee7522618523c199b4d5189040ddb0ccc
Arnaud Tournier <arnaud.tournier@rigpa.org>
2016-06-13 07:09:40 +0200

    et

--- src/HexaBackupReader.ts
+++ src/HexaBackupReader.ts
@@ -34,51 +34,7 @@ export class HexaBackupReader {
         let directoryLister = new DirectoryLister(this.rootPath, this.shaCache, this.ignoredNames);
         await directoryLister.readDir(async (fileDesc) => {
             try {
-                if (fileDesc.isDirectory) {
-                    await store.pushFileDescriptor(this.clientId, transactionId, fileDesc);
-                    return;
-                }
-
-                let fullFileName = fsPath.join(this.rootPath, fileDesc.name);
-
-                let currentSize = await store.hasShaBytes(fileDesc.contentSha);
-                let stat = fs.lstatSync(fullFileName);
-
-                if (currentSize < stat.size) {
-                    const maxBlockSize = 1024 * 100;
-
-                    log(`sending ${stat.size - currentSize} bytes for file ${fileDesc.name} by chunk of ${maxBlockSize}`);
-
-                    let fd = await FsTools.openFile(fullFileName, 'r');
-
-                    let currentReadPosition = currentSize;
-
-                    let gauge = new Gauge()
-                    gauge.show(fileDesc.name, currentReadPosition / stat.size)
-
-                    while (currentReadPosition < stat.size) {
-                        let chunkSize = stat.size - currentReadPosition;
-                        if (chunkSize > maxBlockSize)
-                            chunkSize = maxBlockSize;
-
-                        if (chunkSize > 0) {
-                            let buffer = await FsTools.readFile(fd, currentReadPosition, chunkSize);
-
-                            await store.putShaBytes(fileDesc.contentSha, currentReadPosition, buffer);
-
-                            currentReadPosition += buffer.length;
-
-                            gauge.show(fileDesc.name, currentReadPosition / stat.size)
-                        }
-                    }
-
-                    gauge.hide();
-
-                    await FsTools.closeFile(fd);
-                }
-
-                await store.pushFileDescriptor(this.clientId, transactionId, fileDesc);
-                log(`pushed ${fileDesc.name}`);
+                await this.processFileDesc(store, transactionId, fileDesc)
             }
             catch (error) {
                 log.err(`error reading or pushing ${fileDesc.name} : ${error}`);
@@ -89,6 +45,54 @@ export class HexaBackupReader {
         await store.commitTransaction(this.clientId, transactionId);
 
         log('snapshot sent.');
+    }
+
+    private async processFileDesc(store: IHexaBackupStore, transactionId, fileDesc: Model.FileDescriptor) {
+        if (fileDesc.isDirectory) {
+            await store.pushFileDescriptor(this.clientId, transactionId, fileDesc);
+            return;
+        }
+
+        let fullFileName = fsPath.join(this.rootPath, fileDesc.name);
+
+        let currentSize = await store.hasShaBytes(fileDesc.contentSha);
+        let stat = fs.lstatSync(fullFileName);
+
+        if (currentSize < stat.size) {
+            const maxBlockSize = 1024 * 100;
+
+            log(`sending ${stat.size - currentSize} bytes for file ${fileDesc.name} by chunk of ${maxBlockSize}`);
+
+            let fd = await FsTools.openFile(fullFileName, 'r');
+
+            let currentReadPosition = currentSize;
+
+            let gauge = new Gauge()
+            gauge.show(fileDesc.name, currentReadPosition / stat.size)
+
+            while (currentReadPosition < stat.size) {
+                let chunkSize = stat.size - currentReadPosition;
+                if (chunkSize > maxBlockSize)
+                    chunkSize = maxBlockSize;
+
+                if (chunkSize > 0) {
+                    let buffer = await FsTools.readFile(fd, currentReadPosition, chunkSize);
+
+                    await store.putShaBytes(fileDesc.contentSha, currentReadPosition, buffer);
+
+                    currentReadPosition += buffer.length;
+
+                    gauge.show(fileDesc.name, currentReadPosition / stat.size)
+                }
+            }
+
+            gauge.hide();
+
+            await FsTools.closeFile(fd);
+        }
+
+        await store.pushFileDescriptor(this.clientId, transactionId, fileDesc);
+        log(`pushed ${fileDesc.name}`);
     }
 }
 
