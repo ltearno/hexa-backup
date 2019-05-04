7a199918093524c7d5c4f4bc5fdb78296a65238e
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-05-04 18:00:48 +0200

    wip

--- src/Commands.ts
+++ src/Commands.ts
@@ -1442,7 +1442,16 @@ export async function store(directory: string, port: number, insecure: boolean) 
                 dateWhere += ` and o.lastWrite<=${dateMax}`
             }
 
-            let query = `select o.sha, o.name, o.mimeType${geoSearchSelect} from objects o ${authorizedRefs ? `inner join object_sources os on o.sha=os.sha` : ``}${geoSearchJoin} where ${authorizedRefs ? `os.sourceId in (${authorizedRefs}) and` : ''} (o.name % '${name}' or o.name ilike '%${name}%') and o.mimeType like '${mimeType}'${geoSearchWhere}${dateWhere} group by o.sha, o.name, o.mimeType${geoSearchGroupBy} order by similarity(o.name, '${name}') desc limit 500;`
+            let orderBy = ``
+
+            let nameWhere = 'o.lastWrite'
+            name = name.trim()
+            if (name != '') {
+                nameWhere = ` and (o.name % '${name}' or o.name ilike '%${name}%')`
+                orderBy = `similarity(o.name, '${name}') desc`
+            }
+
+            let query = `select o.sha, o.name, o.mimeType${geoSearchSelect} from objects o ${authorizedRefs ? `inner join object_sources os on o.sha=os.sha` : ``}${geoSearchJoin} where ${authorizedRefs ? `os.sourceId in (${authorizedRefs})` : '1=1'}${nameWhere} and o.mimeType like '${mimeType}'${geoSearchWhere}${dateWhere} group by o.sha, o.name, o.mimeType${geoSearchGroupBy} order by ${orderBy} limit 500;`
             let resultFiles: any = await dbQuery(client, query)
             resultFiles = resultFiles.rows.map(row => ({
                 sha: row.sha,
