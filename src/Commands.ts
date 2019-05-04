77ced9925d4806f1292bc98b0a3566cce49486fa
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-05-04 18:03:42 +0200

    wip

--- src/Commands.ts
+++ src/Commands.ts
@@ -1451,7 +1451,7 @@ export async function store(directory: string, port: number, insecure: boolean) 
                 orderBy = `similarity(o.name, '${name}') desc`
             }
 
-            let query = `select o.sha, o.name, o.mimeType${geoSearchSelect} from objects o ${authorizedRefs ? `inner join object_sources os on o.sha=os.sha` : ``}${geoSearchJoin} where ${authorizedRefs ? `os.sourceId in (${authorizedRefs})` : '1=1'}${nameWhere} and o.mimeType like '${mimeType}'${geoSearchWhere}${dateWhere} group by o.sha, o.name, o.mimeType${geoSearchGroupBy} order by ${orderBy} limit 500;`
+            let query = `select o.sha, o.name, o.mimeType${geoSearchSelect}, min(o.lastWrite) from objects o ${authorizedRefs ? `inner join object_sources os on o.sha=os.sha` : ``}${geoSearchJoin} where ${authorizedRefs ? `os.sourceId in (${authorizedRefs})` : '1=1'}${nameWhere} and o.mimeType like '${mimeType}'${geoSearchWhere}${dateWhere} group by o.sha, o.name, o.mimeType${geoSearchGroupBy} order by ${orderBy} limit 500;`
             let resultFiles: any = await dbQuery(client, query)
             resultFiles = resultFiles.rows.map(row => ({
                 sha: row.sha,
