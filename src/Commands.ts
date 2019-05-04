92e9210c1ce54c1e4af8e48e77f52777d69bd7c6
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-05-04 15:05:57 +0200

    feat

--- src/Commands.ts
+++ src/Commands.ts
@@ -1417,6 +1417,7 @@ export async function store(directory: string, port: number, insecure: boolean) 
                 name: row.name
             }))
 
+            let geoSearchSelect = ''
             let geoSearchJoin = ''
             let geoSearchWhere = ''
             if (mimeType && mimeType.startsWith('image') && geoSearch) {
@@ -1426,16 +1427,19 @@ export async function store(directory: string, port: number, insecure: boolean) 
                 let lngMin = Math.min(nw.lng, se.lng)
                 let lngMax = Math.max(nw.lng, se.lng)
 
+                geoSearchSelect = `, cast(oe.exif ->> 'GPSLatitude' as float) as latitude, cast(oe.exif ->> 'GPSLongitude' as float) as longitude`
                 geoSearchJoin = ` inner join object_exifs oe on o.sha=oe.sha`
                 geoSearchWhere = ` and cast(exif ->> 'GPSLatitude' as float)>=${latMin} and cast(exif ->> 'GPSLatitude' as float)<=${latMax} and cast(exif ->> 'GPSLongitude' as float)>=${lngMin} and cast(exif ->> 'GPSLongitude' as float)<=${lngMax}`
             }
 
-            let query = `select o.sha, o.name, o.mimeType from objects o ${authorizedRefs ? `inner join object_sources os on o.sha=os.sha` : ``}${geoSearchJoin} where ${authorizedRefs ? `os.sourceId in (${authorizedRefs}) and` : ''} (o.name % '${name}' or o.name ilike '%${name}%') and o.mimeType like '${mimeType}'${geoSearchWhere} group by o.sha, o.name, o.mimeType order by similarity(o.name, '${name}') desc limit 500;`
+            let query = `select o.sha, o.name, o.mimeType${geoSearchSelect} from objects o ${authorizedRefs ? `inner join object_sources os on o.sha=os.sha` : ``}${geoSearchJoin} where ${authorizedRefs ? `os.sourceId in (${authorizedRefs}) and` : ''} (o.name % '${name}' or o.name ilike '%${name}%') and o.mimeType like '${mimeType}'${geoSearchWhere} group by o.sha, o.name, o.mimeType order by similarity(o.name, '${name}') desc limit 500;`
             let resultFiles: any = await dbQuery(client, query)
             resultFiles = resultFiles.rows.map(row => ({
                 sha: row.sha,
                 name: row.name,
-                mimeType: row.mimetype
+                mimeType: row.mimetype,
+                lat: row.latitude,
+                lng: row.longitude
             }))
 
             res.send(JSON.stringify({ resultDirectories, resultFilesddd: resultFiles }))
