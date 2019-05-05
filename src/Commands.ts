ac4dc9b116dd044cc55b5730dc67744b3554e12f
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-05-05 08:47:03 +0200

    size

--- src/Commands.ts
+++ src/Commands.ts
@@ -1451,7 +1451,7 @@ export async function store(directory: string, port: number, insecure: boolean) 
                 orderBy = `order by similarity(o.name, '${name}') desc`
             }
 
-            let query = `select o.sha, o.name, o.mimeType${geoSearchSelect}, min(o.lastWrite) as lastWrite from objects o ${authorizedRefs ? `inner join object_sources os on o.sha=os.sha` : ``}${geoSearchJoin} where ${authorizedRefs ? `os.sourceId in (${authorizedRefs})` : '1=1'}${nameWhere} and o.mimeType like '${mimeType}'${geoSearchWhere}${dateWhere} group by o.sha, o.name, o.mimeType${geoSearchGroupBy} ${orderBy} limit 500;`
+            let query = `select o.sha, o.name, o.mimeType${geoSearchSelect}, min(o.size) as size, min(o.lastWrite) as lastWrite from objects o ${authorizedRefs ? `inner join object_sources os on o.sha=os.sha` : ``}${geoSearchJoin} where ${authorizedRefs ? `os.sourceId in (${authorizedRefs})` : '1=1'}${nameWhere} and o.mimeType like '${mimeType}'${geoSearchWhere}${dateWhere} group by o.sha, o.name, o.mimeType${geoSearchGroupBy} ${orderBy} limit 500;`
 
             log(`search query: ${query}`)
 
@@ -1461,6 +1461,7 @@ export async function store(directory: string, port: number, insecure: boolean) 
                 name: row.name,
                 mimeType: row.mimetype,
                 lastWrite: row.lastwrite * 1,
+                size: row.size * 1,
                 lat: row.latitude * 1,
                 lng: row.longitude * 1
             }))
