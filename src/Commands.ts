91ea205de96d43a352ef71ac0b3661c1e8714ec0
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-05-05 13:29:42 +0200

    fix

--- src/Commands.ts
+++ src/Commands.ts
@@ -1453,7 +1453,7 @@ export async function store(directory: string, port: number, insecure: boolean) 
 
             let query = `select o.sha, o.name, o.mimeType${geoSearchSelect}, min(o.size) as size, min(o.lastWrite) as lastWrite from objects o ${authorizedRefs ? `inner join object_sources os on o.sha=os.sha` : ``}${geoSearchJoin} where ${authorizedRefs ? `os.sourceId in (${authorizedRefs})` : '1=1'}${nameWhere} and o.mimeType != 'application/directory' and o.mimeType like '${mimeType}'${geoSearchWhere}${dateWhere} group by o.sha, o.name, o.mimeType${geoSearchGroupBy} ${orderBy} limit 500;`
 
-            log(`search query: ${query}`)
+            log(`search query ${user}: ${query}`)
 
             let resultFiles: any = await dbQuery(client, query)
             resultFiles = resultFiles.rows.map(row => ({
