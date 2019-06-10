9ff14edaf92f0392cd351629f30581eba306c31b
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-06-10 07:26:57 +0200

    manage peer stores

---
+++ src/PeerStores.ts
@@ -0,0 +1,123 @@
+import { LoggerBuilder } from '@ltearno/hexa-js'
+import * as Operations from './Operations'
+import * as ClientPeering from './ClientPeering'
+import { IHexaBackupStore } from './HexaBackupStore'
+import * as RestTools from './RestTools'
+
+const log = LoggerBuilder.buildLogger('peer-stores')
+
+export interface Peer {
+    connection: {
+        ip: string
+        port: number
+        token: string
+        insecure: boolean
+    }
+
+    force: boolean
+    sourceIds: string[]
+}
+
+export class PeerStores {
+    private peers: Peer[] = []
+    private peerIndex = 0
+
+    private delay = 1000 * 60 * 5
+    private timeout: NodeJS.Timeout
+
+    constructor(private store: IHexaBackupStore) { }
+
+    init() {
+        this.schedule()
+    }
+
+    private schedule() {
+        if (this.timeout)
+            clearTimeout(this.timeout)
+        this.timeout = setTimeout(() => this.scheduledTask(), this.delay)
+    }
+
+    async scheduledTask() {
+        try {
+            if (this.peers.length) {
+                this.peerIndex = (this.peerIndex + 1) % this.peers.length
+
+                let peer = this.peers[this.peerIndex]
+
+                if (peer.connection.token) {
+                    // try renew token
+                    let response = await RestTools.post(`https://home.lteconsulting.fr/auth`, JSON.stringify({ token: peer.connection.token }), {
+                        Authorization: `Bearer ${peer.connection.token}`
+                    })
+
+                    if (response.statusCode == 200) {
+                        let parsedResponse = JSON.parse(response.body)
+                        if (parsedResponse && parsedResponse.token) {
+                            log(`new token ${parsedResponse.token}`)
+                            peer.connection.token = parsedResponse.token
+                        }
+                    }
+
+                }
+
+                let ws = await Operations.connectToRemoteSocket(peer.connection.ip, peer.connection.port, peer.connection.token, peer.connection.insecure)
+                if (!ws) {
+                    throw (`connection impossible`)
+                }
+
+                log('connected')
+
+                let peering = new ClientPeering.Peering(ws, false)
+                peering.start().then(_ => log(`finished peering`))
+
+                let remoteStore = peering.remoteStore
+                if (!remoteStore) {
+                    log.err(`cannot connect to remote store`)
+                    return
+                }
+
+                log(`store ready`)
+                log(`transferring`)
+
+                for (let sourceId of peer.sourceIds)
+                    await Operations.pullSource(remoteStore, this.store, sourceId, peer.force)
+
+                log(`pull done`)
+            }
+        }
+        catch (err) {
+            log.err(`error peer-stores scheduled task (${err})`)
+        }
+
+        this.schedule()
+    }
+
+    addEnpointsToApp(app) {
+        app.get('/peers', (req, res) => {
+            res.set('Content-Type', 'application/json')
+            res.send(JSON.stringify(this.peers))
+        })
+
+        app.post('/peers', (req, res) => {
+            res.set('Content-Type', 'application/json')
+            let peer: Peer = req.body
+
+            this.peers.push({
+                connection: {
+                    ip: peer.connection.ip,
+                    port: peer.connection.port,
+                    insecure: peer.connection.insecure,
+                    token: peer.connection.token
+                },
+                force: !!peer.force,
+                sourceIds: peer.sourceIds
+            })
+
+            res.send(JSON.stringify({ message: "ok, added peer" }))
+
+            if (this.timeout)
+                clearTimeout(this.timeout)
+            this.scheduledTask()
+        })
+    }
+}
