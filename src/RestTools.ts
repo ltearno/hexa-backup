814c9d40ef1a7775b9d7e61eb0e733e2e6f87474
Arnaud Tournier <ltearno@gmail.com>
2019-06-26 09:02:43 +0200

    wip

--- src/RestTools.ts
+++ src/RestTools.ts
@@ -1,12 +1,13 @@
 import * as https from 'https'
-import * as http from 'http'
 
 export function post(url: string, data: any, headers: {}): Promise<{ statusCode: number; body: any }> {
     return new Promise((resolve, reject) => {
-        const req = https.request(url, {
-            headers,
-            method: 'POST'
-        }, resp => {
+        const options = {
+            method: 'POST',
+            headers
+        }
+
+        const req = https.request(url, options, resp => {
             let data = ''
             resp.on('data', chunk => data += chunk)
             resp.on('end', () => resolve({ statusCode: resp.statusCode, body: data }))
