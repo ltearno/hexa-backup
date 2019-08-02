122df33a44eca6a85d3a30a61e261373dca1d08b
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-08-02 09:18:59 +0200

    serve content with phantom name

--- src/rest-api/Base.ts
+++ src/rest-api/Base.ts
@@ -23,7 +23,7 @@ export class Base {
 
         app.get('/refs/:id', async (req, res) => {
             res.set('Content-Type', 'application/json')
-            
+
             try {
                 let id = req.params.id
 
@@ -42,68 +42,76 @@ export class Base {
             }
         });
 
-        app.get('/sha/:sha/content', async (req, res) => {
-            res.set('Content-Type', 'application/json')
-            
-            let sha = req.params.sha
-            if (sha == null || sha == 'null') {
-                res.send(`{"error":"input validation (sha is ${sha})"}`)
-                return
+        app.get('/sha/:sha/content', serveShaContent)
+
+        // phantomName is just an easy way for a link to include the desired file name
+        // if the browser wants to download the file (because of mimetype), the file
+        // will have the 'phantomName' instead of 'content
+        app.get('/sha/:sha/content/:phantomName', serveShaContent)
+    }
+}
+
+
+const serveShaContent = async (req, res) => {
+    res.set('Content-Type', 'application/json')
+
+    let sha = req.params.sha
+    if (sha == null || sha == 'null') {
+        res.send(`{"error":"input validation (sha is ${sha})"}`)
+        return
+    }
+
+    // TODO maybe not consider knowing the sha is knowing the existence hence having permission,
+    // and thus make a check against authorized refs.
+
+    const range = req.headers.range
+
+    try {
+        const fileSize = await this.store.hasOneShaBytes(sha)
+
+        if (range) {
+            const parts = range.replace(/bytes=/, "").split("-")
+            const start = parseInt(parts[0])
+            const end = Math.max(start, parts[1] ? parseInt(parts[1], 10) : start == 0 ? Math.min(fileSize - 1, 100 * 1024) : fileSize - 1)
+            const chunksize = (end - start) + 1
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': req.query.type,
+                'Cache-Control': 'private, max-age=31536000',
+                'ETag': sha
             }
 
-            // TODO maybe not consider knowing the sha is knowing the existence hence having permission,
-            // and thus make a check against authorized refs.
+            if (req.query.fileName)
+                head['Content-Disposition'] = `attachment; filename="${req.query.fileName}"`
 
-            const range = req.headers.range
+            res.writeHead(206, head)
+            this.store.readShaAsStream(sha, start, end).pipe(res)
 
-            try {
-                const fileSize = await this.store.hasOneShaBytes(sha)
+            log.dbg(`range-rq ${sha} ${start}-${end}(${parts[1]})/${fileSize}`)
+        }
+        else {
+            if (req.query.type)
+                res.set('Content-Type', req.query.type)
 
-                if (range) {
-                    const parts = range.replace(/bytes=/, "").split("-")
-                    const start = parseInt(parts[0])
-                    const end = Math.max(start, parts[1] ? parseInt(parts[1], 10) : start == 0 ? Math.min(fileSize - 1, 100 * 1024) : fileSize - 1)
-                    const chunksize = (end - start) + 1
-                    const head = {
-                        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-                        'Accept-Ranges': 'bytes',
-                        'Content-Length': chunksize,
-                        'Content-Type': req.query.type,
-                        'Cache-Control': 'private, max-age=31536000',
-                        'ETag': sha
-                    }
+            if (req.query.fileName)
+                res.set('Content-Disposition', `attachment; filename="${req.query.fileName}"`)
 
-                    if (req.query.fileName)
-                        head['Content-Disposition'] = `attachment; filename="${req.query.fileName}"`
+            res.set('ETag', sha)
+            res.set('Cache-Control', 'private, max-age=31536000')
+            res.set('Content-Length', fileSize)
 
-                    res.writeHead(206, head)
-                    this.store.readShaAsStream(sha, start, end).pipe(res)
-
-                    log.dbg(`range-rq ${sha} ${start}-${end}(${parts[1]})/${fileSize}`)
-                }
-                else {
-                    if (req.query.type)
-                        res.set('Content-Type', req.query.type)
-
-                    if (req.query.fileName)
-                        res.set('Content-Disposition', `attachment; filename="${req.query.fileName}"`)
-
-                    res.set('ETag', sha)
-                    res.set('Cache-Control', 'private, max-age=31536000')
-                    res.set('Content-Length', fileSize)
-
-                    this.store.readShaAsStream(sha, 0, -1).pipe(res)
-                }
-            }
-            catch (err) {
-                try {
-                    log.err(`error when sending byte range: ${err}`)
-                    res.send(`{"error":"missing sha ${sha}!"}`)
-                }
-                catch (err2) {
-                    log.err(`error when sending response: ${err2}`)
-                }
-            }
-        })
+            this.store.readShaAsStream(sha, 0, -1).pipe(res)
+        }
+    }
+    catch (err) {
+        try {
+            log.err(`error when sending byte range: ${err}`)
+            res.send(`{"error":"missing sha ${sha}!"}`)
+        }
+        catch (err2) {
+            log.err(`error when sending response: ${err2}`)
+        }
     }
 }
