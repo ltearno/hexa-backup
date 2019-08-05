b78a5c090de21c5968dd80bb3212992e0e50fdce
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-08-05 16:33:57 +0200

    footprints

--- src/DbHelpers.ts
+++ src/DbHelpers.ts
@@ -114,9 +114,20 @@ export async function insertObjectAudioTags(client, sha: string, tags: object) {
     if (!sha || !tags)
         return
 
+    let footprints = []
+    if (tags['common']) {
+        if (tags['common']['artist'])
+            footprints.push(tags['common']['artist'])
+        if (tags['common']['album'])
+            footprints.push(tags['common']['album'])
+        if (tags['common']['title'])
+            footprints.push(tags['common']['title'])
+    }
+    let footprint = footprints.join(' ')
+
     await dbQuery(client, {
-        text: `INSERT INTO object_audio_tags(sha, tags) VALUES($1, $2) ON CONFLICT (sha) DO UPDATE SET tags=$2;`,
-        values: [sha, JSON.stringify(tags)],
+        text: `INSERT INTO object_audio_tags(sha, tags) VALUES($1, $2, $3) ON CONFLICT (sha) DO UPDATE SET tags=$2, footprint=$3;`,
+        values: [sha, JSON.stringify(tags), footprint],
     })
 }
 
