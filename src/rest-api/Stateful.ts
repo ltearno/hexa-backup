407fcda600b98b1ade467d54cd09b6db7f77d6e1
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-08-05 13:13:34 +0200

    yep

--- src/rest-api/Stateful.ts
+++ src/rest-api/Stateful.ts
@@ -35,8 +35,8 @@ export class Stateful {
                             await DbIndexation.updateObjectsIndex(this.store, this.databaseParams)
                             //await DbIndexation.updateMimeShaList('PHOTOS', 'image', store, this.databaseParams)
                             //await DbIndexation.updateMimeShaList('VIDEOS', 'video', store, this.databaseParams)
-                            await DbIndexation.updateExifIndex(this.store, this.databaseParams)
                             await DbIndexation.updateAudioIndex(this.store, this.databaseParams)
+                            await DbIndexation.updateExifIndex(this.store, this.databaseParams)
                             log(`indices updated`)
                         }
                         while (this.runAgainWhenFinished)
