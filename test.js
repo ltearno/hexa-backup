45693a21530d1636e4fe86ede1dfa7f4511f19b7
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-08-05 12:33:06 +0200

    wip

--- test.js
+++ test.js
@@ -2,7 +2,9 @@ const fs = require('fs')
 
 const mm = require('music-metadata')
 
-let buffer = fs.readFileSync('/home/arnaud/Téléchargements/teueyetuyte')
+//let buffer = fs.readFileSync('/home/arnaud/Téléchargements/teueyetuyte')
+//
+let buffer = fs.readFileSync('/media/bigone/hexa-backup/.hb-object/2c/2cf06da869e6d5e0560773466cc9ebed7d83621cebf5f5e0f8ff6fe54e902e11')
 
 //The Roots - The Tipping Point - 06 - Web.mp3
 mm.parseBuffer(buffer, "audio/mp3")
