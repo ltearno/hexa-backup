fed2a080159c301f4144386bdbd37be0ad109839
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-08-05 12:37:45 +0200

    wip

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const mm = require('music-metadata')
 let buffer = fs.readFileSync('/media/bigone/hexa-backup/.hb-object/2c/2cf06da869e6d5e0560773466cc9ebed7d83621cebf5f5e0f8ff6fe54e902e11')
 
 //The Roots - The Tipping Point - 06 - Web.mp3
-mm.parseBuffer(buffer, "audio/mp3")
+mm.parseBuffer(buffer)
     .then(metadata => {
         console.log(`metadata: ${JSON.stringify(metadata)}`, metadata)
     })
