5024ab96fcaa978f1e39106e53951a12832a6ec9
Arnaud Tournier <ltearno@gmail.com>
2019-08-14 09:09:54 +0200

    peers push and pull

--- src/PeerStores.ts
+++ src/PeerStores.ts
@@ -1,7 +1,7 @@
 import { LoggerBuilder } from '@ltearno/hexa-js'
 import * as Operations from './Operations'
 import * as ClientPeering from './ClientPeering'
-import { HexaBackupStore } from './HexaBackupStore'
+import { HexaBackupStore, IHexaBackupStore } from './HexaBackupStore'
 import * as RestTools from './RestTools'
 
 const log = LoggerBuilder.buildLogger('peer-stores')
@@ -16,6 +16,7 @@ export interface Peer {
 
     force: boolean
     sourceIds: string[]
+    push: boolean
 }
 
 export class PeerStores {
@@ -30,11 +31,19 @@ export class PeerStores {
     async init() {
         this.schedule()
 
+        await this.loadPeers()
+    }
+
+    private async loadPeers() {
         this.peers = await this.store.getReferenceRepository().getEx(`peers`, `peers`)
         if (!this.peers) {
             this.peers = []
         }
         else {
+            this.peers.forEach(peer => {
+                peer.push = !!peer.push
+            })
+
             log(`loaded peers : ${JSON.stringify(this.peers)}`)
         }
     }
@@ -47,10 +56,7 @@ export class PeerStores {
 
     async scheduledTask() {
         try {
-            this.peers = await this.store.getReferenceRepository().getEx(`peers`, `peers`)
-            if (!this.peers) {
-                this.peers = []
-            }
+            await this.loadPeers()
 
             if (this.peers.length) {
                 this.peerIndex = (this.peerIndex + 1) % this.peers.length
@@ -79,16 +85,29 @@ export class PeerStores {
                 }
 
                 log(`store ready`)
-                log(`transferring`)
+
+                let sourceStore: IHexaBackupStore = remoteStore
+                let destinationStore: IHexaBackupStore = this.store
+
+                if (peer.push) {
+                    let temp = sourceStore
+                    sourceStore = destinationStore
+                    destinationStore = temp
+
+                    log(`transferring local to remote`)
+                }
+                else {
+                    log(`transferring remote to local`)
+                }
 
                 let sourceIds = []
                 if (peer.sourceIds)
                     sourceIds = peer.sourceIds
                 else
-                    sourceIds = await remoteStore.getSources()
+                    sourceIds = await sourceStore.getSources()
 
                 for (let sourceId of sourceIds)
-                    await Operations.pullSource(remoteStore, this.store, sourceId, peer.force)
+                    await Operations.pullSource(sourceStore, destinationStore, sourceId, peer.force)
 
                 log(`pull done`)
             }
