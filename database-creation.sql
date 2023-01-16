5d57da030882756281491186e0fa721d9951c708
Arnaud TOURNIER <ltearno@gmail.com>
2023-01-16 07:31:59 +0100

    wip

--- database-creation.sql
+++ database-creation.sql
@@ -1,12 +1,15 @@
 CREATE EXTENSION pg_trgm;
 CREATE EXTENSION fuzzystrmatch;
-create table objects (sha char(64), isDirectory boolean, lastWrite bigint, size bigint, name text, mimeType text);
-alter table objects add constraint objects_unique primary key (sha, isDirectory, lastWrite, size, name, mimeType);
-CREATE INDEX trgm_idx_objects_name ON objects USING gin (name gin_trgm_ops);
-CREATE index idx_objects_sha on objects USING btree (sha);
-CREATE index idx_objects_mimeType on objects USING btree (mimeType);
-create index idx_objects_lastWrite on objects USING btree(lastWrite);
-CREATE index idx_objects_size on objects USING btree (size);
+
+create table objects_hierarchy (sourceId char(64), parentSha char(64), sha char(64), lastWrite bigint, size bigint, name text, mimeType text);
+alter table objects_hierarchy add constraint objects_hierarchy_unique primary key (sourceId, parentSha, sha, lastWrite, size, name, mimeType);
+CREATE index idx_objects_hierarchy_sourceId on objects_hierarchy USING btree (sourceId);
+CREATE index idx_objects_hierarchy_parentSha on objects_hierarchy USING btree (parentSha);
+CREATE index idx_objects_hierarchy_sha on objects_hierarchy USING btree (sha);
+create index idx_objects_hierarchy_lastWrite on objects_hierarchy USING btree(lastWrite);
+CREATE index idx_objects_hierarchy_size on objects_hierarchy USING btree (size);
+CREATE INDEX trgm_idx_objects_hierarchy_name ON objects_hierarchy USING gin (name gin_trgm_ops);
+CREATE index idx_objects_hierarchy_mimeType on objects_hierarchy USING btree (mimeType);
 
 /* object_footprints : sha names, and (artist, album, genre,... for audio/ objects) */
 /*
@@ -24,13 +27,3 @@ create index idx_object_footprints_sha on objects USING btree (sha);
 /* content related metadata */
 create table object_audio_tags (sha char(64), tags jsonb, primary key (sha));
 create table object_exifs (sha char(64), exif jsonb, primary key (sha));
-
-create table objects_hierarchy (sourceId char(64), parentSha char(64), sha char(64), lastWrite bigint, size bigint, name text, mimeType text);
-alter table objects_hierarchy add constraint objects_hierarchy_unique primary key (sourceId, parentSha, sha, lastWrite, size, name, mimeType);
-CREATE index idx_objects_hierarchy_sourceId on objects_hierarchy USING btree (sourceId);
-CREATE index idx_objects_hierarchy_parentSha on objects_hierarchy USING btree (parentSha);
-CREATE index idx_objects_hierarchy_sha on objects_hierarchy USING btree (sha);
-create index idx_objects_hierarchy_lastWrite on objects_hierarchy USING btree(lastWrite);
-CREATE index idx_objects_hierarchy_size on objects_hierarchy USING btree (size);
-CREATE INDEX trgm_idx_objects_hierarchy_name ON objects_hierarchy USING gin (name gin_trgm_ops);
-CREATE index idx_objects_hierarchy_mimeType on objects_hierarchy USING btree (mimeType);
